a103d103bcdfe606fedeba4e94e4d3656af0121b
PB14209127 <fan123@mail.ustc.edu.cn>
2017-12-18 19:48:30 +0800

    report

--- PB14209127-project3/source/random.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RANDOM_FILE_
-#define _RANDOM_FILE_
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<cstdlib>
-void creatfile(const std::string&path,int lenth);
-//creating the random file
-#endif
